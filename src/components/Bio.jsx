e82940ea4ea57ab95e9bf5c01af6058de24acd53
Ishmael-Williams <80982790+Ishmael-Williams@users.noreply.github.com>
2022-06-12 12:42:34 -0400

    formatting changes

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,13 +1,14 @@
 import "./Bio.css";
 
 function Bio() {
+  //https://docs.google.com/document/d/14EadMnSbemHrlYOAvImE9T-dNGwOvCHw/edit?usp=sharing&ouid=103907712850444278130&rtpof=true&sd=true
   return (
     <div className={"Bio"}>
       <text className={"Bio-name-text"}>Ishmael Williams</text>
       <p className={"Bio-tagline-text"}></p>
       <a
         className={"anchor"}
-        href="https://docs.google.com/document/d/14EadMnSbemHrlYOAvImE9T-dNGwOvCHw/edit?usp=sharing&ouid=103907712850444278130&rtpof=true&sd=true"
+        href="https://drive.google.com/file/d/1Z5dsmzVmYgzq_em8c5JHuUykLh6llsC0/view?usp=sharing"
         target="_blank"
         rel="noreferrer noopener"
       >
