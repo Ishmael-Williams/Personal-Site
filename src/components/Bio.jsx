7679eb2ec8226aa2bb9b01b02e8e452ca851fcdf
Ishmael-Williams <80982790+Ishmael-Williams@users.noreply.github.com>
2023-03-16 18:09:38 -0400

    Revert "large refactor, added new icons"
    This reverts commit 8b925298b03ac7f8850b5c4b9b339273ff7b136f.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,24 +1,35 @@
 import "./Bio.css";
-import BioButton from './BioButton.jsx';
 
 function Bio() {
+  //https://docs.google.com/document/d/14EadMnSbemHrlYOAvImE9T-dNGwOvCHw/edit?usp=sharing&ouid=103907712850444278130&rtpof=true&sd=true
   return (
     <div className={"Bio"}>
       <text className={"Bio-name-text"}>Ishmael Williams</text>
-      <p className={"Bio-tagline-text"}>KSU Computer Science Graduate</p>
-      <p className={"Bio-tagline-text"}>ishmaelwilliams777@gmail.com</p>
-      <BioButton 
-        text={"Resume"}
-        href={"https://drive.google.com/file/d/1Z5dsmzVmYgzq_em8c5JHuUykLh6llsC0/view?usp=sharing"}
-      />
-      <BioButton 
-        text={"LinkedIn"}
-        href={"https://www.linkedin.com/in/ishmael-williams"}
-      />
-      <BioButton 
-        text={"GitHub"}
-        href={"https://github.com/Ishmael-Williams"}
-      />
+      <p className={"Bio-tagline-text"}></p>
+      <a
+        className={"anchor"}
+        href="https://drive.google.com/file/d/1Z5dsmzVmYgzq_em8c5JHuUykLh6llsC0/view?usp=sharing"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <button className={"btn1"}>Resume</button>
+      </a>
+      <a
+        className={"anchor"}
+        href="https://www.linkedin.com/in/ishmael-williams/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <button className={"btn1"}>LinkedIn</button>
+      </a>
+      <a
+        className={"anchor"}
+        href="https://github.com/Ishmael-Williams"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <button className={"btn1"}>GitHub </button>
+      </a>
     </div>
   );
 }
