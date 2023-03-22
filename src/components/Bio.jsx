bea0a0f22af9d06aff2c33cacb59eda4ea1782bc
Ishmael-Williams <80982790+Ishmael-Williams@users.noreply.github.com>
2023-03-22 18:30:46 -0400

    Added icons, refactored buttons, made text larger

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,5 +1,6 @@
 import "./Bio.css";
-import BioButton from './BioButton.jsx';
+import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
+import { FaGoogleDrive } from "react-icons/fa";
 
 function Bio() {
   return (
@@ -7,18 +8,40 @@ function Bio() {
       <text className={"Bio-name-text"}>Ishmael Williams</text>
       <p className={"Bio-tagline-text"}>KSU Computer Science Graduate</p>
       <p className={"Bio-tagline-text"}>ishmaelwilliams777@gmail.com</p>
-      <BioButton 
-        text={"Resume"}
+
+      <a
         href={"https://drive.google.com/file/d/1Z5dsmzVmYgzq_em8c5JHuUykLh6llsC0/view?usp=sharing"}
-      />
-      <BioButton 
-        text={"LinkedIn"}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <button className="portfolioBtn">
+          <FaGoogleDrive className ="icon" size="1.7em"/>
+          {"Resume"}
+        </button>
+      </a>
+
+      <a
         href={"https://www.linkedin.com/in/ishmael-williams"}
-      />
-      <BioButton 
-        text={"GitHub"}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <button className="portfolioBtn">
+          <AiFillLinkedin className="icon" size="1.7em"/>
+          {"LinkedIn"}
+        </button>
+      </a>
+
+      <a
         href={"https://github.com/Ishmael-Williams"}
-      />
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <button className="portfolioBtn">
+          <AiOutlineGithub  className="icon" size="1.7em"/>
+          {"GitHub"}
+        </button>
+      </a>
+
     </div>
   );
 }
